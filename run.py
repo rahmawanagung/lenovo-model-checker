e4ae737d7eb0e43227dd8537e23ab2e3ceab0bcd
rahmawanagung <agungedi001@gmail.com>
2025-07-01 13:19:58 +0700

    feat: ubah link localhost

--- run.py
+++ run.py
@@ -5,5 +5,5 @@ from app import app  # Mengimpor object 'app' dari file app.py
 # Host '0.0.0.0' berarti server bisa diakses dari luar localhost
 # Port 8080 adalah port umum untuk aplikasi web
 if __name__ == '__main__':
-    print("Server produksi dijalankan pada http://0.0.0.0:8080")
-    serve(app, host='0.0.0.0', port=8080)
+    print("Server produksi dijalankan pada http://localhost:8080/")
+    serve(app, host='localhost', port=8080)
